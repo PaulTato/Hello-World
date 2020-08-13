ffe63032905f2fd7a1848945935eef49513d7717
PaulTato <69611791+PaulTato@users.noreply.github.com>
2020-08-13 10:53:20 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Paul Truong.");
+		System.out.println("My hobbies include playing video games and tinkering with random electronics.");
+		System.out.println("My favorite video games are Minecraft, osu! and League of Legends.");
+		System.out.println("I want to go to a college or university somewhere in California.");
+		System.out.println("I hope to maybe major in electrical engineering or computer engineering.");
+		System.out.println("I hope to develop basic knowledge in Java and improve my typing speed by taking this class.");
+	}
+}
